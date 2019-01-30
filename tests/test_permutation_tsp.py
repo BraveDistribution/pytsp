6e1db2e125cccbbc7a6652b74bdbd08b2b2eb42c
BraveDistribution <gazda.matej@gmail.com>
2019-01-30 10:16:33 +0100

    Import error, datastructures transformed into module

--- tests/test_permutation_tsp.py
+++ tests/test_permutation_tsp.py
@@ -1,8 +1,10 @@
 import numpy as np
 
 from pytsp.permutations_tsp import permutations_tsp
+from pytsp.utils import route_cost
 
-def test_3_opt_algorithm():
+
+def test_permutation_algorithm():
     graph = np.array([[0, 5, 2, 1, 1, 4],
                       [5, 0, 6, 2, 2, 5],
                       [2, 6, 0, 7, 4, 2],
@@ -12,5 +14,21 @@ def test_3_opt_algorithm():
     route = [0, 1, 2, 3, 4, 5]
     result = permutations_tsp(graph)
     print(result)
-    assert True == True
+    assert route_cost(graph,route) > route_cost(graph,result)
 
+
+def test_permutation_algorithm_2():
+    graph = np.array([[0, 4, 3, 4, 5, 1, 2, 3, 4],
+                       [4, 0, 1, 4, 3, 4, 6, 2, 1],
+                       [3, 1, 0, 1, 4, 3, 2, 1, 9],
+                       [4, 4, 1, 0, 4, 6, 1, 2, 3],
+                       [5, 3, 4, 4, 0, 1, 2, 5, 3],
+                       [1, 4, 3, 6, 1, 0, 2, 5, 3],
+                       [2, 6, 2, 1, 2, 2, 0, 3, 5],
+                       [3, 2, 1, 2, 5, 5, 3, 0, 9],
+                       [4, 1, 9, 3, 3, 3, 5, 0, 0]
+                       ])
+    route = [0,1,2,3,4,5,6,7,8]
+    result = permutations_tsp(graph)
+    print(route_cost(graph, result))
+    assert route_cost(graph, route) > route_cost(graph,result)
