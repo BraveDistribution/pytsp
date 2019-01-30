9236ad3d35b9e54fe0083cf8a0e85b4b6c233c7b
BraveDistribution <gazda.matej@gmail.com>
2019-01-30 14:23:33 +0100

    updated setup.py

--- setup.py
+++ setup.py
@@ -43,6 +43,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/BraveDistribution/pytsp',
-    version='0.1.7',
+    version='0.2.0',
     zip_safe=False,
 )
